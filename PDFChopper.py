1aa93bbb8510b992343f0e7216bb91587bf4cab6
Brainium.ai <brainiumai3@gmail.com>
2023-06-02 13:22:14 +0700

    fix pdf issue, start-over

--- PDFChopper.py
+++ PDFChopper.py
@@ -1,7 +1,10 @@
 
+import logging
 import os
 import PyPDF2
 from PyPDF2 import PdfReader, PdfWriter
+
+logging.basicConfig(level=logging.DEBUG)
 
 """
 Takes a pdf path and splits it based on size 
@@ -11,21 +14,99 @@ TODO: cmd prompt args vs prompt
 print("Chop it up!")
 
 test_file = "./pdfs/100.pdf"
+logging.debug(f'name of test file: {test_file}')
 
-def read_pdf_content(path_to_pdf):
-    with open(path_to_pdf, 'rb') as pdf:
+
+def get_pdf_metadata(pdf_path):
+    with open(pdf_path, 'rb') as pdf:
         reader = PdfReader(pdf)
-        results = []
-        for page_num in range(0, len(reader.pages)):
-            selected_content = reader.pages[page_num]
-            results.append(selected_content)
-
-    return ' '.join(results)
+        metadata = reader.metadata
+        logging.debug(f'{repr(metadata)} is the metadata')
+        logging.debug(f'Producer: {metadata.producer}, \n Author: {repr(metadata.author)}, Title: {repr(metadata.title)}')
+        return metadata
 
 
-def chop_pdf_by_size(desired_size_in_mb):
-    pass
+get_pdf_metadata('pdfs/school.pdf')
 
 
-def chop_pdf_by_pages(desired_page_range):
-    pass
+def get_file_length(pdf_path):
+    return len(PdfReader(pdf_path).pages)
+
+
+logging.debug(f"{get_file_length('pdfs/school.pdf')} pages detected")
+
+
+
+
+
+
+
+# def read_pdf_content(path_to_pdf, start_page=0, end_page=-1):
+#     """
+#     returns the text content of the pdf within the bounds set
+#     default is all
+#     """
+#     with open(path_to_pdf, 'rb') as pdf:
+#         reader = PdfReader(pdf)
+#         results = []
+#         for page_num in range(0, len(reader.pages)):
+#             selected_page = reader.pages[page_num]
+#             text = selected_page.extract_text()
+#             # if text:
+#                 # logging.debug(f'text found on page {page_num}')
+#             results.append(text)
+
+#         return ' '.join(results)
+
+
+# def chop_pdf_by_pages(
+#         path_to_pdf, pages, start_page=0, output_pdf_name="Chopped_PDF"
+#         ): 
+   
+#     """
+#     takes a pdf and chops it into the desired number of pages
+#     returning the new chopped-up pdfs  
+#     """
+
+#     end_page_number = start_page + pages
+
+#     logging.debug(
+#         f'{path_to_pdf} is the path; extract pages {start_page} - {end_page_number}, \n{output_pdf_name}: name')
+    
+#     print('hi')
+    
+# read_pdf_content('pdfs/100.pdf')
+# chop_pdf_by_pages('pdfs/100.pdf', 3)
+
+
+
+
+
+
+
+
+    
+#     #     #  TODO: previous work is commented below, think need a function and can use recursion to call itself from the previous end point
+#     # with open(path_to_pdf, 'wb') as f:
+#     #     reader = PdfReader(f)
+
+
+#         # writer = PdfWriter()
+#         # results = []
+#         # for page_num in range(start_page, end_page_number):
+#         #     current_page = reader.pages[page_num]
+
+#         #     # TODO: fix, this just chops to page num need to keep going in chunks
+#         #     """
+#         #     add the chunks to the writer until its full, then reset the end_page_number again but continuing from where it left off. 
+#         #     recursion?
+
+#         #     """
+
+#         #     current_page_content = current_page.extract_text()
+#         #     results.append(current_page_content)
+#         #     writer.add_page(current_page)
+#         #     logging.debug(f'writer: {writer}, results: {results}')
+
+#         #     with open(output_pdf_name, 'wb') as output_pdf:
+#         #         writer.write(output_pdf)
